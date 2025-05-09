761fa8bc9d3fae6003802619f82c2cddff6a8ca7
shivangimajestic <shivangi@majestictechnologies.net>
2025-05-09 10:06:06 +0530

    report approvals

--- DB/Message.cs
+++ DB/Message.cs
@@ -10,16 +10,11 @@ public class Message
 {
     [Key]
     public int Id { get; set; }
-
     public int ReceivingReportId { get; set; }
-
     public string Sender { get; set; } = null!; 
     public string Recipient { get; set; } = null!; 
-
     public string Content { get; set; } = null!;
-
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
     [ForeignKey("ReceivingReportId")]
     public virtual ReceivingReport ReceivingReport { get; set; } = null!;
 }
--- Pages/ViewReceivingReport.cshtml.cs
+++ Pages/ViewReceivingReport.cshtml.cs
@@ -9,12 +9,20 @@ namespace LabMaterials.Pages
         public string ReportId { get; set; }
 
         public string ReceivingWarehouse;
+        public string ReceipientManager;
+
+        public string TechnicalMember;
+        public string GeneralSupervisor;
+
         public ViewReceivingReportModel(LabDBContext context)
         {
             _context = context;
         }
 
         public List<ReceivingReport> ReceivingReports { get; set; }
+        public List<ReceivingItem> ReceivingItems {get; set;}
+        public List<Item> Items {get; set;}
+        public List<Unit> Units {get; set;}
 
         public List<Store> Stores {get; set;}
         public ReceivingReport? Report { get; set; }
@@ -28,15 +36,30 @@ namespace LabMaterials.Pages
             }
             var dbContext = new LabDBContext();
             Stores = dbContext.Stores.ToList();
+            Items = dbContext.Items.ToList();
+            Units =  dbContext.Units.ToList();
 
             Report = await _context.ReceivingReports
                     .Include(r => r.Supplier)
                         .FirstOrDefaultAsync(r => r.Id == int.Parse(this.ReportId));
 
+            ReceivingItems = dbContext.ReceivingItems.Where(r => r.ReceivingReportId == Report.Id).ToList();
+
             #pragma warning disable CS8601
-                        this.ReceivingWarehouse = dbContext.Stores.Where(s => s.StoreId == int.Parse(Report.ReceivingWarehouse)).Select(s => s.StoreName)
+                this.ReceivingWarehouse = dbContext.Stores.Where(s => s.StoreId == int.Parse(Report.ReceivingWarehouse)).Select(s => s.StoreName)
                         .FirstOrDefault();
+           
+                this.ReceipientManager = dbContext.Users.Where(u => u.JobNumber == Report.RecipientEmployeeId).Select(s => s.FullName).FirstOrDefault();
+
+                this.TechnicalMember = dbContext.Users.Where(u => u.UserId == Report.TechnicalMemberId).Select(s => s.FullName).FirstOrDefault();
+
+                var GeneralSupervisor = dbContext.Users.FirstOrDefault(u => u.UserId == Report.ChiefResponsibleId);
+
+                if(GeneralSupervisor != null){
+                    this.GeneralSupervisor = GeneralSupervisor.FullName;
+                }
             #pragma warning restore CS8601 
+
 
             // ReceivingReports = await _context.ReceivingReports
             //     .Include(r => r.Supplier)
