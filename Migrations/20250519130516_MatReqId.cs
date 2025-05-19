e96d1560f8c6ed906b07d646cd51975d18078c7d
shivangimajestic <shivangi@majestictechnologies.net>
2025-05-19 18:50:13 +0530

    deduction order

---
+++ Migrations/20250519130516_MatReqId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabMaterials.Migrations
+{
+    /// <inheritdoc />
+    public partial class MatReqId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaterialRequestId",
+                table: "PendingDeductions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaterialRequestId",
+                table: "PendingDeductions");
+        }
+    }
+}
