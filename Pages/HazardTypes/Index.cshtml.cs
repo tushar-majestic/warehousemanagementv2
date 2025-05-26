b3088f4d8b9619a32d1fc404cd1f06607c11e934
SakshiBalodiya <sakshi@majestictechnologies.net>
2025-05-26 14:04:09 +0530

    store user

--- Pages/HazardTypes/Index.cshtml.cs
+++ Pages/HazardTypes/Index.cshtml.cs
@@ -13,11 +13,20 @@ namespace LabMaterials.Pages_HazardTypes
         }
 
         public IList<HazardType> HazardType { get; set; } = default!;
-
+        public string lblUsers, lblHazardTypeName, lblAction, lblHazardType;
         public async Task OnGetAsync()
         {
+            FillLables();
             base.ExtractSessionData();
             HazardType = await _context.HazardTypes.ToListAsync();
         }
+        private void FillLables()
+        {
+            this.lblHazardType = (Program.Translations["HazardType"])[Lang];
+            this.lblUsers = (Program.Translations["Users"])[Lang];
+            this.lblHazardTypeName = (Program.Translations["HazardTypeName"])[Lang];
+            this.lblAction = (Program.Translations["Action"])[Lang];
+        }
+        
     }
 }
