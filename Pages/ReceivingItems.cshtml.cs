09c5f8a9496d100be324e5d1573ca642f1ee5d04
shivangimajestic <shivangi@majestictechnologies.net>
2025-05-27 16:08:41 +0530

    changes

---
+++ Pages/ReceivingItems.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LabMaterials.DB;
+using System.Linq;
+using LabMaterials.DB;
+using LabMaterials.dtos;
+
+
+namespace LabMaterials.Pages
+{
+    public class ReceivingItemsModel : BasePageModel
+    {
+    }
+}
