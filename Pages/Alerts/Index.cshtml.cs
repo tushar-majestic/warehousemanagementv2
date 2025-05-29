8c92e96d1a9e7da40d450090b014d62fed1c651c
tushar-majestic <tushar@majestictechnologies.net>
2025-05-29 14:28:47 +0530

    alertpage

---
+++ Pages/Alerts/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LabMaterials.Pages.Alerts
+{
+    public class IndexModel : BasePageModel
+    {
+        private readonly LabMaterials.DB.LabDBContext _context;
+
+        public IndexModel(LabMaterials.DB.LabDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ItemCard> ItemCardminimum { get; set; } = default!;
+        public IList<ItemCard> ItemCardCeiling { get; set; } = default!;
+        public IList<ItemCard> ItemCardReorder { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            base.ExtractSessionData();
+            ItemCardminimum = await _context.ItemCards.Where(i => i.QuantityAvailable < i.Minimum)
+                .Include(i => i.GroupCodeNavigation)
+                .Include(i => i.HazardTypeNameNavigation)
+                .Include(i => i.Item)
+                .Include(i => i.ItemTypeCodeNavigation)
+                .Include(i => i.Store).ToListAsync();
+
+            ItemCardCeiling = await _context.ItemCards.Where(i => i.QuantityAvailable >= i.Ceiling)
+                .Include(i => i.GroupCodeNavigation)
+                .Include(i => i.HazardTypeNameNavigation)
+                .Include(i => i.Item)
+                .Include(i => i.ItemTypeCodeNavigation)
+                .Include(i => i.Store).ToListAsync();
+
+            ItemCardReorder = await _context.ItemCards.Where(i => i.QuantityAvailable <= i.ReorderLimit)
+                .Include(i => i.GroupCodeNavigation)
+                .Include(i => i.HazardTypeNameNavigation)
+                .Include(i => i.Item)
+                .Include(i => i.ItemTypeCodeNavigation)
+                .Include(i => i.Store).ToListAsync();
+        }
+    }
+}
